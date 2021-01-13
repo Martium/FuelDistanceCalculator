c599136ef3099a5aca7afcd4cd9c6b4934a02091
Erikas Neverdauskas <erikasnever@hotmail.com>
2021-01-13 18:27:00 +0200

    rename app

---
+++ Martium.TravelInfo/Forms/TravelInfoForm.cs
@@ -0,0 +1,21 @@
+﻿using System.Windows.Forms;
+
+namespace Martium.TravelInfo.Forms
+{
+    public partial class TravelInfoForm : Form
+    {
+        public TravelInfoForm()
+        {
+            InitializeComponent();
+
+            InitializeControls();
+        }
+
+        private void InitializeControls()
+        {
+            TripPriceTextBox.Enabled = false;
+            DistanceTextBox.Enabled = false;
+            CalculateButton.Enabled = false;
+        }
+    }
+}
---
+++ Martium.TravelInfo/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Windows.Forms;
+using Martium.TravelInfo.Forms;
+
+namespace Martium.TravelInfo
+{
+    static class Program
+    {
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new TravelInfoForm());
+        }
+    }
+}
