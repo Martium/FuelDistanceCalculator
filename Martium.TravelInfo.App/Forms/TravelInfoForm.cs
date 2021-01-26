9a66fcca6328f086ff86bc5e96494dea6e31ea1a
Martynas Gedutis <martynasgedutis@gmail.com>
2021-01-26 13:18:27 +0200

    removed

--- Martium.TravelInfo.App/Forms/TravelInfoForm.cs
+++ Martium.TravelInfo.App/Forms/TravelInfoForm.cs
@@ -72,7 +72,6 @@ namespace Martium.TravelInfo.App.Forms
             else
             {
                 _messageDialogService.ShowErrorDialog("Nepavyko rasti įvesto adreso, praŠome patikslinkite arba įveskite kitą");
-                DepartureAddressTextBox.Focus();
             }
 
             ToggleButtonStateForStringTextBox(DepartureAddressTextBox, SaveDepartureAddressButton, _travelInfoSettingsModel.DepartureAddress);
