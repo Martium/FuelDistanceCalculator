014038a76d7118b839ed521e766dd56b615b7040
Erikas Neverdauskas <erikasnever@hotmail.com>
2021-01-30 12:20:29 +0200

    moved api client to same project

---
+++ Martium.TravelInfo.App/Contracts/RouteInfoSummary.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Martium.TravelInfo.App.Contracts
+{
+    public class RouteInfoSummary
+    {
+        public string DistanceUnit { get; set; }
+        public double TotalDistanceInKm { get; set; }
+        public string DurationUnit { get; set; }
+        public TimeSpan TotalDuration { get; set; }
+    }
+}
